38a7b5f8b4a673b792e8b7d6cf58dc18cfad5abf
Igor <igorchesalin@gmail.com>
2018-10-01 19:44:16 +0300

    queries.sql исправления-2

--- queries.sql
+++ queries.sql
@@ -2,14 +2,14 @@ INSERT INTO users (email, password) VALUES
   ("igor@gmail.com", "12345"),
   ("masha@gmail.com", "54321");
 
-INSERT INTO projects (title, user_id) VALUES
+INSERT INTO projects (title, users_id) VALUES
   ("Входящие", 1),
   ("Учеба", 1),
   ("Работа", 1),
   ("Домашние дела", 1),
   ("Авто", 1);
 
-INSERT INTO tasks (user_id, project_id, create_date, deadline, done_date, title, file) VALUES
+INSERT INTO tasks (users_id, projects_id, create_date, deadline, done_date, title, file) VALUES
   (1, 3, "2018-09-01", "2018-10-15", NULL, "Собеседование в IT компании", NULL),
   (1, 3, "2018-09-05", "2018-09-30", NULL,  "Выполнить тестовое задание", NULL),
   (1, 2, "2018-09-07", "2018-09-27", "2018-09-20", "Сделать задание первого раздела", NULL),
@@ -18,10 +18,10 @@ INSERT INTO tasks (user_id, project_id, create_date, deadline, done_date, title,
   (1, 4, "2018-09-10", NULL, NULL, "Заказать пиццу", NULL);
 
 -- получить список из всех проектов для одного пользователя;
-SELECT title FROM projects WHERE user_id = 1;
+SELECT title FROM projects WHERE users_id = 1;
 
 -- получить список из всех задач для одного проекта;
-SELECT title FROM tasks WHERE project_id = 3;
+SELECT title FROM tasks WHERE projects_id = 3;
 
 -- пометить задачу как выполненную;
 UPDATE tasks SET done_date = "2018-09-27" WHERE id = 4;
